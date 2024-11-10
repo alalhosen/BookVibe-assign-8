aa4e80b39974c14002265c34f39fce1d667e78fe
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-10 20:53:15 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <div>
+             <h1 className="text-3xl font-bold text-lime-500">
+         # BookVibe!
+      </h1>
+            <p>Bangladesh</p>
+            <button className="btn">Button</button>
+        </div>
+    );
+};
+
+export default Home;
