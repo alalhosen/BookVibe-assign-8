bf1f89c70cc4d18e8d3caf9fbdf8ed95582b6c71
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-16 23:36:33 +0600

    footer section add

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -7,10 +7,10 @@ const Banner = () => {
                     src="https://i.ibb.co.com/Fx7CZy6/61lh-BP2-Uf9-L-SY466.jpg"
                     className="rounded-lg shadow-2xl" />
                 <div className="pr-24">
-                    <h1 className="text-5xl py- font-bold mb-4 mt-4">Books to freshen up your bookshelf</h1>
+                    <h1 className="text-6xl py- font-bold mb-4 mt-8">Books to freshen up your bookshelf</h1>
 
                     <button className=" 
-                    btn mt-6 text-xl px-6 py-3 font-semibold rounded-lg bg-green-500 text-white">View The List</button>
+                    btn mt-12 text-xl px-6 py-3 font-semibold rounded-lg bg-green-500 text-white">View The List</button>
                 </div>
             </div>
         </div>
