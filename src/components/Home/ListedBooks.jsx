9cb97f622f1687c361f71afbd14f29723bcf6deb
Alal Hosen <alalhosen1992620120@gmail.com>
2024-12-01 18:43:17 +0600

    create layout component

--- src/components/Home/ListedBooks.jsx
+++ src/components/Home/ListedBooks.jsx
@@ -1,7 +1,11 @@
+import Nav from "./Nav";
 
 const ListedBooks = () => {
     return (
         <div>
+            <div className="h-16">
+                <Nav />
+            </div>
             <h1>This is Listed books.</h1>
         </div>
     );
